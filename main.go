8bd931169d20cf8927a397c32dbe37c975bd2e3f
kingparks <137748916+kingparks@users.noreply.github.com>
2024-04-23 10:26:19 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"github.com/gin-gonic/gin"
+)
+
+var staticRoot string //静态文件根目录
+var port string       //端口
+
+func main() {
+	flag.StringVar(&staticRoot, "s", "", "静态文件根目录")
+	flag.StringVar(&port, "p", "", "端口")
+
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Static("/static", staticRoot)
+	r.Run(port)
+}
